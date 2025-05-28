4c30559dc1f52b341965f761520a111f729471d7
Luc <52007751+pixe71@users.noreply.github.com>
2025-05-28 14:27:15 +0200

    Update chatmoderator.lua

--- chatmoderator.lua
+++ chatmoderator.lua
@@ -6,20 +6,20 @@ local forbiddenwords = {"example 1" , "example 2"}
 -- Function to detect player messages
 hook.Add("PlayerSay", "ModérationChat", function(ply, text, team)
   
-    local texteMinuscules = string.lower(text)
+    local lowercasetext = string.lower(text)
 
     -- Checks if the word appears in the table
-    for _, motInterdit in ipairs(forbiddenwords) do
-        if string.find(texteMinuscules, motInterdit) then
+    for _, wordForbidden in ipairs(forbiddenwords) do
+        if string.find(lowercasetext, wordForbidden) then
             -- Use the correct function according to your admin mode
             if ULib and ULib.ban then
                 -- Use ulx
-                local raison = "Chat-Moderator ---> You have been automatically banned by the server for your behavior in the chat."
-                ULib.ban(ply, 120, raison)
+                local reason = "Chat-Moderator ---> You have been automatically banned by the server for your behavior in the chat."
+                ULib.ban(ply, 120, reason)
             elseif SAM then
            --Use sam admin
-                local raison = "Chat-Moderator ---> You have been automatically banned by the server for your behavior in the chat."
-                game.ConsoleCommand("!samtempban " .. ply:SteamID() .. " 120 " .. raison .. "\n")
+                local reason = "Chat-Moderator ---> You have been automatically banned by the server for your behavior in the chat."
+                game.ConsoleCommand("!samtempban " .. ply:SteamID() .. " 120 " .. reason .. "\n")
             end
 
             -- Make sure messages are not displayed 
